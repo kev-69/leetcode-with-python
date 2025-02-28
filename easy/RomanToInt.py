450556f560acde40423560a51af629f572f4b9ab
kev-69 <bismarkobuobi19@gmail.com>
2025-02-28 18:18:34 +0000

    Fixed the errors and this solution was accepted

--- easy/RomanToInt.py
+++ easy/RomanToInt.py
@@ -1,18 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        # Corrected the mapping: "X" instead of "x" and "M" should map to 1000
         roman_values = {
             "I": 1,
             "V": 5,
-            "x": 10,
+            "X": 10,  # Changed from "x" to "X"
             "L": 50,
             "C": 100,
             "D": 500,
-            "M": 100
+            "M": 1000  # Corrected value for "M"
         }
+
         result = 0
         for i in range(len(s)):
             if i + 1 < len(s) and roman_values[s[i]] < roman_values[s[i + 1]]:
                 result -= roman_values[s[i]]
             else:
                 result += roman_values[s[i]]
+
         return result
